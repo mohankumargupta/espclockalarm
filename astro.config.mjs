91ba9a39d0a818c9620f2b566e61d3066018ee89
Mohan Gupta <mohangupta@live.com>
2026-04-29 14:45:14 +1000

    brainstorming

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,12 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'ESP32 AI Clock Alarm',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/mohankumargupta/espclockalarm' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Brainstorming',
+					autogenerate: { directory: 'brainstorming' },
 				},
 			],
 		}),
